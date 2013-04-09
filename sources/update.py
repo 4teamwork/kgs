b88a6e7d742b56dabd911098ebe5bd5247706eb4
Jonas Baumann <jone@jone.ch>
2013-04-09 10:51:06 +0200

    Sources: support custom sources / branches entries.

--- sources/update.py
+++ sources/update.py
@@ -8,61 +8,71 @@ from ConfigParser import ConfigParser
 BASEPATH = os.path.abspath(os.path.dirname(__file__))
 
 
-TEMPLATE = '''# DO NOT MODIFY
+PREFIX = '''# DO NOT MODIFY
 # This file is auto generated, use the update script!
 
-[buildout]
-extends = http://plonesource.org/sources.cfg
-
-[branches]
-%(branches)s
-
-[sources]
-%(sources)s
 '''
 
 
-def get_repos():
+def get_config():
     parser = ConfigParser()
 
     with open(os.path.join(BASEPATH, 'config.ini')) as file_:
         parser.readfp(file_)
 
+    return parser
+
+
+def get_repos():
+    config = get_config()
     return dict(map(lambda item: (item[0], item[1].strip().split()),
-                    parser.items('repos')))
+                    config.items('repos')))
 
 
-def get_branches(repos):
-    lines = sorted(map(lambda name: '%s = master' % name.split('/')[-1],
-                       reduce(list.__add__, repos.values())))
-    return '\n'.join(lines)
+def write_branches(repos, output):
+    sorted(map(lambda name: output.set('branches',
+                                       name.split('/')[-1], 'master'),
+               reduce(list.__add__, repos.values())))
 
 
-def get_sources(repos):
-    lines = []
-
+def write_sources(repos, output):
     for name in repos['gitolite']:
         shortname = name.split('/')[-1]
-        lines.append('%s = git gitolite@git.4teamwork.ch:%s.git'
-                     ' branch=${branches:%s}' % (
-                shortname, name, shortname))
+
+        output.set(
+            'sources', shortname,
+            'git gitolite@git.4teamwork.ch:%s.git'
+            ' branch=${branches:%s}' % (
+                name, shortname))
 
     for name in repos['github_private']:
-        lines.append(
-            '%s = git git@github.com:4teamwork/%s.git'
+        output.set(
+            'sources', name,
+            'git git@github.com:4teamwork/%s.git'
             ' branch=${branches:%s}' % (
-                name, name, name))
+                name, name))
 
-    return '\n'.join(sorted(lines))
 
+config = get_config()
+output = ConfigParser()
+output.add_section('buildout')
+output.set('buildout', 'extends', 'http://plonesource.org/sources.cfg')
+
+# copy branches and sources sections
+output.add_section('branches')
+for key, value in config.items('branches'):
+    output.set('branches', key, value)
+
+output.add_section('sources')
+for key, value in config.items('sources'):
+    output.set('sources', key, value)
 
 repos = get_repos()
-branches = get_branches(repos)
-sources = get_sources(repos)
+branches = write_branches(repos, output)
+sources = write_sources(repos, output)
 
 with open(os.path.join(BASEPATH, 'sources.cfg'), 'w+') as file_:
-    file_.write(TEMPLATE % {
-            'branches': branches,
-            'sources': sources})
+    file_.write(PREFIX)
+    output.write(file_)
 
 print 'UPDATED'
